0021913637ea6530c60680c877d871073526765a
xiaochenggit <707996290@qq.com>
2017-03-22 17:04:25 +0800

    搭建博客配置
    搭建博客 配置

--- 重学nodejs/一起学node.js/myblog/index.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var app = new express();
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user');
-app.use('/user',userRouter);
-app.use('*',indexRouter);
-app.listen(3000);
--- 重学nodejs/一起学node.js/myblog/routes/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-router.get("/",function (req, res){
-	res.send('我是首页');
-})
-module.exports = router;
