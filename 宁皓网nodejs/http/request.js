6e34ead075e7d50d51200073144c4e01b96006cf
xiaochenggit <707996290@qq.com>
2017-03-27 21:31:27 +0800

    rt
    rt

--- 宁皓网nodejs/http/request.js
+++ 宁皓网nodejs/http/request.js
@@ -1,33 +1,25 @@
-const http = require('http');
-
-// var options = {
-// 	protocol: 'http:',
-// 	hostname: 'api.douban.com',
-// 	port: '80',
-// 	path: '/v2/movie/top250'
-// };
+var http= require('http'); 
 var options = { 
 hostname: 'www.google.com', 
 port: 80, 
 path: '/upload', 
 method: 'POST' 
 }; 
-var responseDate = '';
-var request = http.request(options, (response) => {
-	console.log(response);
-	response.setEncoding('utf8');
-	response.on('data', (chunk) => {
-		responseDate += chunk;
-	})
-	response.on('end', () => {
-		console.log(JSON.parse(responseDate));
-	})
-});
-
-request.on('error', (error) => {
-	console.log(error);
-})
-
-request.on('end', () => {
-	console.log('结束');
-})
+ 
+var req = http.request(options, function(res) { 
+console.log('STATUS: ' + res.statusCode); 
+console.log('HEADERS: ' + JSON.stringify(res.headers)); 
+res.setEncoding('utf8'); 
+res.on('data', function (chunk) { 
+console.log('BODY: ' + chunk); 
+}); 
+}); 
+ 
+req.on('error', function(e) { 
+console.log('problem with request: ' + e.message); 
+}); 
+ 
+// write data to request body 
+req.write('data\n'); 
+req.write('data\n'); 
+req.end();
